fc9102ac0b84e62c59f9e8e28b888ed0dcc685fd
yokeshkumar18 <yokeshk868@gmail.com>
2025-08-12 14:54:30 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", addTask);
+
+taskInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") addTask();
+});
+
+function addTask() {
+  const taskText = taskInput.value.trim();
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  li.addEventListener("click", () => {
+    li.classList.toggle("completed");
+  });
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "✖";
+  deleteBtn.classList.add("delete-btn");
+  deleteBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    taskList.removeChild(li);
+  });
+
+  li.appendChild(deleteBtn);
+  taskList.appendChild(li);
+  taskInput.value = "";
+}
